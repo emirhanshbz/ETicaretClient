69cd9d9af9aca438cd23f1cd00f772ea6fe5b0fc
emirhan <emir0sahbaz@gmail.com>
2025-02-26 22:28:36 +0300

    Error Response

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -27,11 +27,35 @@ export class CreateComponent extends BaseComponent implements OnInit {
     create_product.stock = parseInt(stock.value);
     create_product.price = parseFloat(price.value);
 
+    if(!name.value) {
+      this.alertify.message("Ürün Adı Boş Olamaz", {
+        dismissOthers: false,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      });
+      return;
+    }
+
+    if(parseInt(stock.value) < 0) {
+      this.alertify.message("Stok 0'dan az olamaz", {
+        dismissOthers: false,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      });
+      return;
+    }
+
     this.productService.create(create_product, () => {
       this.hideSpinner(SpinnerType.BallSpinClockwiseFade);
       this.alertify.message("Ürün Başarıyla Eklendi", {
         dismissOthers: true,
         messageType: MessageType.Success,
+        position: Position.TopRight
+      });
+    },errorMessage => {
+      this.alertify.message(errorMessage, {
+        dismissOthers: true,
+        messageType: MessageType.Error,
         position: Position.TopRight
       });
     });
