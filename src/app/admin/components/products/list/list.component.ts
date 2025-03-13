f71320d750710927ad11aa1f36d0e4fd72e21c07
emirhan <emir0sahbaz@gmail.com>
2025-03-13 22:55:41 +0300

    Delete Directive Düzeltmeleri

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -28,7 +28,7 @@ export class ListComponent extends BaseComponent implements OnInit {
 
   async getProducts() {
     this.showSpinner(SpinnerType.BallSpinClockwiseFade);
-    const allProducts: { totalCount: number; products: List_Product[] } = await this.productService.read(this.paginator ? this.paginator.pageIndex : 0, this.paginator ? this.paginator.pageSize : 5, () => this.hideSpinner(SpinnerType.BallSpinClockwiseFade), errorMessage => this.alertifyService.message(errorMessage, {
+    const allProducts: { totalCount: number; products: List_Product[] } = await this.productService.read(this.paginator ? this.paginator.pageIndex : 0, this.paginator ? this.paginator.pageSize : 5, () => this.hideSpinner(SpinnerType.BallPulseSync), errorMessage => this.alertifyService.message(errorMessage, {
       dismissOthers: true,
       messageType: MessageType.Error,
       position: Position.TopRight
--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -5,6 +5,8 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from '../../base/base.component';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from '../../services/admin/alertify.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 declare var $: any;
 
@@ -17,9 +19,10 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService,
+    private httpClientService: HttpClientService,
     private spinner: NgxSpinnerService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private alertifyService: AlertifyService
   ) {
     const img = _renderer.createElement("img");
     img.setAttribute("src", "assets/delete_1.png");
@@ -30,19 +33,37 @@ export class DeleteDirective {
    }
 
    @Input() id: string;
+   @Input() controller: string;
    @Output() callBack: EventEmitter<any> = new EventEmitter();
 
    @HostListener("click")
    async onClick() {
     this.openDialog(async () => {
+      this.spinner.show(SpinnerType.BallPulseSync);
       const td: HTMLTableCellElement = this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).animate({
-        opacity: 0,
-        left: "+=50",
-        height: "toggle"
-      }, 700,() => {
-        this.callBack.emit();
+      //await this.productService.delete(this.id);
+      this.httpClientService.delete({
+        controller: this.controller
+      },this.id).subscribe(data => {
+        $(td.parentElement).animate({
+          opacity: 0,
+          left: "+=50",
+          height: "toggle"
+        }, 700,() => {
+          this.callBack.emit();
+          this.alertifyService.message("Silme işlemi başarılı", {
+            dismissOthers: true,
+            messageType: MessageType.Success,
+            position: Position.TopRight
+          })
+        });
+      }, (errorResponse: HttpErrorResponse) => {
+        this.spinner.hide(SpinnerType.BallPulseSync);
+        this.alertifyService.message("Silme işlemi başarısız!", {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        })
       });
     });
    }
