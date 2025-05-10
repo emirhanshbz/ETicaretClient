88b94470569e72ff634c5b2b75962983fb9e5d87
emirhan <emir0sahbaz@gmail.com>
2025-05-10 18:12:41 +0300

    Guard Yapılanması İle Yetkisiz Erişim Engellemesi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 import { LayoutComponent } from './admin/layout/layout.component';
 import { HomeComponent } from './ui/components/home/home.component';
+import { AuthGuard } from './guards/common/auth.guard';
 
 const routes: Routes = [
   {
     path: "admin", component: LayoutComponent, children: [
-      { path:"", component: DashboardComponent }, //https://....com/admin sayfası için varsayılan sayfayı dashboard yaptık
+      { path:"", component: DashboardComponent, canActivate: [AuthGuard] }, //https://....com/admin sayfası için varsayılan sayfayı dashboard yaptık
 
       { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then
-      (module => module.CustomerModule) },  //https://....com/admin/customers e bir istek geliyorsa bu modülde ara
+      (module => module.CustomerModule), canActivate: [AuthGuard] },  //https://....com/admin/customers e bir istek geliyorsa bu modülde ara
     
       { path: "products", loadChildren: () => import("./admin/components/products/products.module").then
-      (module => module.ProductsModule) },
+      (module => module.ProductsModule), canActivate: [AuthGuard] },
 
       { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then
-      (module => module.OrderModule) },
-    ]
+      (module => module.OrderModule), canActivate: [AuthGuard] },
+    ], canActivate: [AuthGuard] // admin sayfasına erişim için auth guard'ı kullanıyoruz
   },
   { path: "", component: HomeComponent},
   { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule)},
---
+++ src/app/guards/common/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../services/ui/custom-toastr.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+
+  constructor(private jwtHelper: JwtHelperService, private router: Router, private toastrService: CustomToastrService) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+      const token: string = localStorage.getItem("accessToken");
+      
+      //const decodeToken = this.jwtHelper.decodeToken(token);
+      //const expirationDate = this.jwtHelper.getTokenExpirationDate(token);
+      let expired: boolean;
+      try {
+        expired = this.jwtHelper.isTokenExpired(token);
+      } catch {
+        expired = true;
+      }
+
+      if(!token || expired) {
+        this.router.navigate(["login"], { queryParams: { returnUrl: state.url}});
+        this.toastrService.message("Oturum açmanız gerekiyor", "Yetkisiz Erişim", {
+          messageType: ToastrMessageType.Warning,
+          position: ToastrPosition.TopRight
+        })
+      }
+
+      //debugger;
+
+      return true;
+  }
+}
